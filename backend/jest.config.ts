0cdb793ef7917f619435545fb98b22497ecfecef
mehul raheja <mehul3127@gmail.com>
2023-09-12 01:17:03 +0530

    password reset template, forgot password, reset password, jest config and jest script

---
+++ backend/jest.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  // check official documentation for below configuration details
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true, //if it is false then it will only print when the test is completed
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testMatch: ['<rootDir>/src/**/test/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/test/*.ts?(x)', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1
+    }
+  },
+  coverageReporters: ['text-summary', 'lcov'],
+  // we are using custom paths in the project to run and we need to define here as well so that tests can also use those paths
+  moduleNameMapper: {
+    '@auth/(.*)': ['<rootDir>/src/features/auth/$1'],
+    '@user/(.*)': ['<rootDir>/src/features/user/$1'],
+    '@global/(.*)': ['<rootDir>/src/shared/globals/$1'],
+    '@service/(.*)': ['<rootDir>/src/shared/services/$1'],
+    '@socket/(.*)': ['<rootDir>/src/shared/sockets/$1'],
+    '@worker/(.*)': ['<rootDir>/src/shared/workers/$1'],
+    '@root/(.*)': ['<rootDir>/src/$1'],
+  }
+};
+
+export default config;
