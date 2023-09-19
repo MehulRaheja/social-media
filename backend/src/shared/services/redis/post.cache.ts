97b9ad57ba0a4f590ff45f7114ab8eb0d1c3999d
mehul raheja <mehul3127@gmail.com>
2023-09-20 00:27:49 +0530

    post with image controller, create post unit test, get posts from redis method

--- backend/src/shared/services/redis/post.cache.ts
+++ backend/src/shared/services/redis/post.cache.ts
@@ -1,10 +1,14 @@
 import { ServerError } from '@global/helpers/error-handler';
-import { ISavePostToCache } from '@post/interfaces/post.interface';
+import { Helpers } from '@global/helpers/helpers';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
+import { IPostDocument, IReactions, ISavePostToCache } from '@post/interfaces/post.interface';
 import { config } from '@root/config';
 import { BaseCache } from '@service/redis/base.cache';
 import Logger from 'bunyan';
 
 const log: Logger = config.createLogger('postCache');
+
+export type PostCacheMultiType = string | number | Buffer | RedisCommandRawReply[] | IPostDocument | IPostDocument[];
 
 export class PostCache extends BaseCache {
   constructor() {
@@ -69,6 +73,36 @@ export class PostCache extends BaseCache {
       const count: number = parseInt(postCount[0], 10) + 1;
       multi.HSET(`users:${currentUserId}`, 'postsCount', count);
       multi.exec(); // this will execute all the methods which are chained with multi
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. try again.');
+    }
+  }
+
+  // we implement pagination here because we don't want to get all the posts at once
+  // we want to get posts in reverse order latest first
+  public async getPostsFromCache(key: string, start: number, end: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      // returns the array of zadd ids
+      const reply: string[] = await this.client.ZRANGE(key, start, end, { REV: true });
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for(const value of reply) {
+        multi.HGETALL(`posts:${value}`);
+      }
+      const replies: PostCacheMultiType = (await multi.exec()) as PostCacheMultiType;
+      const postReplies: IPostDocument[] = [];
+      for(const post of replies as IPostDocument[]){
+        post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+        post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+        post.createdAt = new Date(Helpers.parseJson(`${post.createdAt}`)) as Date;
+        postReplies.push(post);
+      }
+
+      return postReplies;
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. try again.');
