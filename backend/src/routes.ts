31856f016f310f4d5388227c45450ffdec7c65e1
mehul raheja <mehul3127@gmail.com>
2023-10-30 23:44:54 +0530

    add image queue, profile and background image controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { currentUserRoutes } from '@auth/routes/currentRoutes';
 import { commentRoutes } from '@comment/routes/commentRoutes';
 import { follwerRoutes } from '@follower/routes/followerRoutes';
 import { authMiddleware } from '@global/helpers/auth.middleware';
+import { imageRoutes } from '@image/routes/imageRoutes';
 import { notificationRoutes } from '@notification/routes/notificationRoutes';
 import { postRoutes } from '@post/routes/postRoutes';
 import { reactionRoutes } from '@reaction/routes/reactionRoutes';
@@ -25,6 +26,7 @@ export default (app: Application) => {
     app.use(BASE_PATH, authMiddleware.verifyUser, commentRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, follwerRoutes.routes());
     app.use(BASE_PATH, authMiddleware.verifyUser, notificationRoutes.routes());
+    app.use(BASE_PATH, authMiddleware.verifyUser, imageRoutes.routes());
   };
   routes();
 };
