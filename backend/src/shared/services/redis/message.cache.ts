1eab60523a8740151d41dbe61bf7cf612c7f81cd
mehul raheja <mehul3127@gmail.com>
2023-11-02 22:49:29 +0530

    get chat messages

--- backend/src/shared/services/redis/message.cache.ts
+++ backend/src/shared/services/redis/message.cache.ts
@@ -2,7 +2,7 @@ import { config } from '@root/config';
 import Logger from 'bunyan';
 import { BaseCache } from '@service/redis/base.cache';
 import { ServerError } from '@global/helpers/error-handler';
-import { findIndex } from 'lodash';
+import { find, findIndex } from 'lodash';
 import { IChatList, IChatUsers, IMessageData } from '@chat/interfaces/chat.interface';
 import { Helpers } from '@global/helpers/helpers';
 
@@ -100,6 +100,31 @@ export class MessageCache extends BaseCache {
         conversationChatList.push(Helpers.parseJson(lastMessage));
       }
       return conversationChatList;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+
+  public async getChatMessagesFromCache(senderId: string, receiverId: string): Promise<IMessageData[]> {
+    try {
+      if(!this.client.isOpen){
+        await this.client.connect();
+      }
+      const userChatList: string[] = await this.client.LRANGE(`chatList:${senderId}`, 0, -1);
+      const receiver: string = find(userChatList, (listItem: string) => listItem.includes(receiverId)) as string;
+      const parsedReceiver: IChatList = Helpers.parseJson(receiver) as IChatList;
+      if(parsedReceiver){ // above 3 lines will not be required if we pass conversation id as a parameter to the function
+        const userMessages: string[] = await this.client.LRANGE(`messages:${parsedReceiver.conversationId}`, 0, -1);
+        const chatMessages: IMessageData[] = [];
+        for(const item of userMessages) {
+          const chatItem = Helpers.parseJson(item) as IMessageData;
+          chatMessages.push(chatItem);
+        }
+        return chatMessages;
+      } else {
+        return [];
+      }
     } catch (error) {
       log.error(error);
       throw new ServerError('Server error. Try again');
