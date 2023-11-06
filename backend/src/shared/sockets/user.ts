7f7d5817cdb5038f714c9b0ec33f3a3ae8a27700
mehul raheja <mehul3127@gmail.com>
2023-11-06 22:02:32 +0530

    add user map socket

--- backend/src/shared/sockets/user.ts
+++ backend/src/shared/sockets/user.ts
@@ -1,9 +1,11 @@
 // we will directly send data to the user, so that user don't have to wait for data to get saved on redis and mongodb
-import { ISocketData } from '@user/interfaces/user.interface';
+import { ILogin, ISocketData } from '@user/interfaces/user.interface';
 import { Server, Socket } from 'socket.io';
 
 // we will use this object outside this file
 export let socketIOUserObject: Server;
+export const connectedUsersMap: Map<string, string> = new Map(); // it contains the ids of currently active users
+let users: string[] = [];
 
 export class SocketIOUserHandler {
   private io: Server;
@@ -17,6 +19,10 @@ export class SocketIOUserHandler {
     // whatever event we want to use it needs to be add inside this connection
     // events emitted here will go to every user
     this.io.on('connection', (socket: Socket) => {
+      socket.on('setup', (data: ILogin) => {
+        this.addClientToMap(data.userId, socket.id);
+      });
+
       socket.on('block user', (data: ISocketData) => {
         this.io.emit('blocked user id', data); // whenever user unfollow we will emit an event and it is going to every user not just a particular socket
       });
@@ -24,6 +30,26 @@ export class SocketIOUserHandler {
       socket.on('unblock user', (data: ISocketData) => {
         this.io.emit('unblocked user id', data); // whenever user unfollow we will emit an event and it is going to every user not just a particular socket
       });
+
+      socket.on('disconnect', () => {
+        this.removeClientFromMap(socket.id);
+      });
     });
+  }
+
+  private addClientToMap(userId: string, socketId: string): void {
+    if(!connectedUsersMap.has(userId)) {
+      connectedUsersMap.set(userId, socketId);
+    }
+  }
+
+  private removeClientFromMap(socketId: string): void {
+    if(Array.from(connectedUsersMap.values()).includes(socketId)) {
+      const disconnectedUser: [string, string] = [...connectedUsersMap].find((user: [string, string]) => {
+        return user[1] === socketId;
+      }) as [string, string];
+      connectedUsersMap.delete(disconnectedUser[0]);
+      // send event to the client
+    }
   }
 }
