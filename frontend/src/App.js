31f40dca493101ab7e41b4ec66de80c0077eeac8
mehul raheja <mehul3127@gmail.com>
2023-12-14 13:15:23 +0530

    socket setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from '@root/routes';
 import '@root/App.scss';
+import { useEffect } from 'react';
+import { socketService } from '@services/socket/socket.service';
 
 const App = () => {
+  useEffect(() => {
+    socketService.setupSocketConnection();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
