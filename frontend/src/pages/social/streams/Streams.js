b6350d3038653a6bc139db9ce168300dd1e28795
mehul raheja <mehul3127@gmail.com>
2023-12-17 23:54:34 +0530

    Post form, post reducer and post modal

--- frontend/src/pages/social/streams/Streams.js
+++ frontend/src/pages/social/streams/Streams.js
@@ -4,6 +4,7 @@ import Suggestions from '@components/suggestions/Suggestions';
 import { useDispatch } from 'react-redux';
 import { getUserSuggestions } from '@redux/api/suggestion';
 import useEffectOnce from '@hooks/useEffectOnce';
+import PostForm from '@components/posts/post-form/PostForm';
 
 const Streams = () => {
   const bodyRef = useRef(null);
@@ -18,7 +19,7 @@ const Streams = () => {
     <div className="streams" data-testid="streams">
       <div className="streams-content">
         <div className="streams-post" ref={bodyRef} style={{ backgroundColor: 'white' }}>
-          <div>Post Form</div>
+          <PostForm />
           <div>Post Items</div>
           <div ref={bottomLineRef} style={{ marginBottom: '50px', height: '50px' }}></div>
         </div>
