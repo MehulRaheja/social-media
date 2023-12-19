d67c4bbb207c82df6878b6fb783ef62914853eef
mehul raheja <mehul3127@gmail.com>
2023-12-20 00:05:07 +0530

    clear post image method

--- frontend/src/services/utils/post-utils.service.js
+++ frontend/src/services/utils/post-utils.service.js
@@ -1,5 +1,5 @@
 import { closeModal } from '@redux/reducers/modal/modal.reducer';
-import { clearPost } from '@redux/reducers/post/post.reducer';
+import { clearPost, updatePostItem } from '@redux/reducers/post/post.reducer';
 
 export class PostUtils {
   static selectBackground(bgColor, postData, setTextAreaBackground, setPostData, setDisable) {
@@ -18,5 +18,35 @@ export class PostUtils {
   static closePostModal(dispatch) {
     dispatch(closeModal());
     dispatch(clearPost());
+  }
+
+  static clearImage(postData, post, inputRef, dispatch, setSelectedPostImage, setPostImage, setDisable, setPostData) {
+    postData.gifUrl = '';
+    postData.image = '';
+    setSelectedPostImage(null);
+    setPostImage('');
+    setDisable(false);
+    setTimeout(() => {
+      if (inputRef?.current) {
+        inputRef.current.textContent = !post ? postData?.post : postData;
+        if (post) {
+          postData.post = post;
+        }
+        setPostData(postData);
+      }
+    });
+    dispatch(updatePostItem({ gifUrl: '', image: '', imgId: '', imgVersion: '' }));
+  }
+
+  static postInputData(imageInputRef, postData, post, setPostData) {
+    setTimeout(() => {
+      if (imageInputRef?.current) {
+        imageInputRef.current.textContent = !post ? postData?.post : postData;
+        if (post) {
+          postData.post = post;
+        }
+        setPostData(postData);
+      }
+    });
   }
 }
