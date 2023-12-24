4e3d306d225593eebe331fc1fdd714dfeaaab864
mehul raheja <mehul3127@gmail.com>
2023-12-24 22:59:54 +0530

    unit tests related to post and its compnents

--- frontend/src/pages/social/streams/StreamsSkeleton.js
+++ frontend/src/pages/social/streams/StreamsSkeleton.js
@@ -1,6 +1,7 @@
 import '@pages/social/streams/Streams.scss';
-import SuggestionsSkeletons from '@components/suggestions/SuggestionSkeleton';
 import PostFormSkeleton from '@components/posts/post-form/PostFormSkeleton';
+import PostSkeleton from '@components/posts/post/PostSkeleton';
+import SuggestionsSkeletons from '@components/suggestions/SuggestionSkeleton';
 
 const StreamsSkeleton = () => {
   return (
@@ -9,7 +10,9 @@ const StreamsSkeleton = () => {
         <div className="streams-post">
           <PostFormSkeleton />
           {[1, 2, 3, 4, 5, 6].map((index) => (
-            <div key={index}>Posts Items</div>
+            <div key={index}>
+              <PostSkeleton />
+            </div>
           ))}
         </div>
         <div className="streams-suggestions">
