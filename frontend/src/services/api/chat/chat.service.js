4e03527c1c756c4eae4a7a6f75cb007bea1e9a19
mehul raheja <mehul3127@gmail.com>
2024-04-15 00:27:09 +0530

    chat Utils Service

--- frontend/src/services/api/chat/chat.service.js
+++ frontend/src/services/api/chat/chat.service.js
@@ -5,6 +5,12 @@ class ChatService {
     const response = await axios.get('/chat/message/conversation-list');
     return response;
   }
+
+  // to remove chat users
+  async removeChatUsers(body) {
+    const response = await axios.get('/chat/message/remove-chat-users', body);
+    return response;
+  }
 }
 
 export const chatService = new ChatService();
